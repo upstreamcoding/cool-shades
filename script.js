d4ea3eb48a9f0c55b6efd032760ba7223850dc5e
Alan Mabry <alan.mabry@gmail.com>
2019-03-26 20:23:47 -0400

    data

---
+++ script.js
@@ -0,0 +1,26 @@
+var redShades = {
+  title: 'Red',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-red.jpg',
+  price: 99
+};
+var greenShades = {
+  title: 'Green',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-green.jpg',
+  price: 99
+};
+var blueShades = {
+  title: 'Blue',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-blue.jpg',
+  price: 99
+};
+var blackShades = {
+  title: 'Black',
+  subtitle: 'Rock-N-Stroll',
+  img: 'images/product-black.jpg',
+  price: 99
+};
+
+var shades = [redShades, greenShades, blueShades, blackShades];
