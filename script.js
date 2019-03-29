6217f15aca26ab1a12cda6209361db704f4bf637
Alan Mabry <alan.mabry@gmail.com>
2019-03-28 20:43:05 -0400

    click circle log associated boject

--- script.js
+++ script.js
@@ -25,13 +25,20 @@ var blackShades = {
 
 var shades = [redShades, greenShades, blueShades, blackShades];
 
-
 function mobileProducts(product) {
   return `
     <div class="col peppers-product text-center">
       <img class="img-fluid" src="${product.img}" alt="Peppers Rock-N-Stroll Red">
       <p class="product-title">${product.subtitle}</p>
       <h3>${product.title}</h3>
+
+      <div class="circles">
+        <div class="circle red" data-index="0"></div>
+        <div class="circle green" data-index="1"></div>
+        <div class="circle blue" data-index="2"></div>
+        <div class="circle black" data-index="3"></div>
+      </div>
+
       <p class="product-price">$${product.price}</p>
       <div class="product-cta-container">
         <a href="#" class="btn btn-danger btn-lg">Add to Cart</a>
@@ -42,4 +49,10 @@ function mobileProducts(product) {
 
 $(document).ready(function() {
   $('#mobile-products').html(mobileProducts(blackShades));
+
+  $('.circle').click(function(){
+    var index = $(this).data('index');
+
+    console.log(shades[index]);
+  });
 });
